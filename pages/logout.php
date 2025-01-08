eab85398b2f758993a03278cd14438d7a5460920
salma hamdi <saalmahm51@gmail.com>
2025-01-08 12:51:16 +0100

    Quelques modifications de page logout

--- pages/logout.php
+++ pages/logout.php
@@ -1,7 +1,15 @@
 <?php
 session_start();
-require_once '/classes/Authentication.php';
-$auth = new Authentication(null);  
+
+require_once '/classes/Database.php'; 
+require_once '/classes/Authentication.php';  
+
+$db = new Database();
+$conn = $db->conn;
+
+// Passer la connexion à la classe Authentication
+$auth = new Authentication($conn); 
+
 $auth->logout();
 header('Location: ./login.php');
 exit;
